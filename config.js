bbd87a9b11384e61b9e1578bf4a87e9d008d0dd7
David Maillard <david@dmaillard.com>
2018-04-07 09:41:52 -0300

    UaiCoin

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
 var api = 'http://18.232.33.114:21898';
-var donationAddress = "qsqs";
+var donationAddress = "";
 var blockTargetInterval = 30;
 var coinUnits = 100;
-var symbol = 'trtl';
+var symbol = 'UAI';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
  "trtl": [
-	["z-pool.com", "http://z-pool.com:8117"],
-	["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
-	["us.turtlepool.space", "http://us.turtlepool.space:8117"],
-	["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
-	["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
-	["slowandsteady.fun", "http://slowandsteady.fun:8119"],
-	["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
-	["auspool.turtleco.in", "https://auspool.turtleco.in/api"],
-	["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
-	["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
-	["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
-	["xk.is", "https://xk.is/api"]
+	// ["z-pool.com", "http://z-pool.com:8117"],
+	// ["eu.turtlepool.space", "http://eu.turtlepool.space:8117"],
+	// ["us.turtlepool.space", "http://us.turtlepool.space:8117"],
+	// ["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
+	// ["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
+	// ["slowandsteady.fun", "http://slowandsteady.fun:8119"],
+	// ["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
+	// ["auspool.turtleco.in", "https://auspool.turtleco.in/api"],
+	// ["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
+	// ["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
+	// ["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
+	// ["xk.is", "https://xk.is/api"]
  ]
 };
 
